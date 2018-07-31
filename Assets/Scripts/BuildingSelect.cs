60130fa984b5c1878c91d633535d28d53bd8c6ae
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-07-31 21:49:30 +0700

    add raycast to enable object select

---
+++ Assets/Scripts/BuildingSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.transform.name == "Cube")
+            {
+
+            }
+        }
+	}
+}
