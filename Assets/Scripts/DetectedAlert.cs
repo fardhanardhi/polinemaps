7b3dd3a57690670261dfbd9dc9675165fb107d4c
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-08-02 19:41:50 +0700

    fix alert with multi target

---
+++ Assets/Scripts/DetectedAlert.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System.Linq;
+
+public class DetectedAlert : MonoBehaviour {
+    public Transform PanelScanMarker;
+    public Transform PanelMenu;
+
+    // Use this for initialization
+    void Start () {
+        PanelScanMarker.gameObject.SetActive(true);
+        PanelMenu.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        StateManager sm = TrackerManager.Instance.GetStateManager();
+        IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours();
+        if (activeTrackables.Count() > 0)
+        {
+            PanelScanMarker.gameObject.SetActive(false);
+            PanelMenu.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            PanelScanMarker.gameObject.SetActive(true);
+            PanelMenu.gameObject.SetActive(false);
+        }
+    }
+}
